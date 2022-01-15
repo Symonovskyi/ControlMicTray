20c3eaa9c028afbb11b371a87a6a87d453651b52
unknown <timoxa.or12@gmail.com>
2022-01-15 15:55:20 +0200

    Working program in alpha state!

--- trayIcon.py
+++ trayIcon.py
@@ -1,8 +1,11 @@
 import sys
-#import ctypes
+import win32api
+import win32gui
 from ctypes import windll
 from PyQt5 import QtGui, QtWidgets
 
+WM_APPCOMMAND = 0x319
+APPCOMMAND_MICROPHONE_VOLUME_MUTE = 0x180000
 
 class Main(QtWidgets.QSystemTrayIcon):
     def __init__(self):
@@ -10,25 +13,28 @@ class Main(QtWidgets.QSystemTrayIcon):
         # Два состояния - 1 и 0
         self.status = bool()
 
-        icon = QtGui.QIcon("icon.ico")
-
         self.menu = QtWidgets.QMenu()
-        turnMicroOn = self.menu.addAction("Включить микрофон")
+        turnMicroOn = self.menu.addAction("Вкл\выкл. микрофон")
+        turnMicroOn.triggered.connect(self.Unmute)
 
-        turnMicroOff = self.menu.addAction("Выключить микрофон")
-
-        quantityOfActiveMics = self.menu.addAction(f"{windll.winmm.waveInGetNumDevs()}")
+        quantityOfActiveMics = self.menu.addAction(
+            f"Кол-ство микрофонов: {windll.winmm.waveInGetNumDevs()}")
 
         exitAction = self.menu.addAction("Выход")
         exitAction.triggered.connect(sys.exit)
 
-        self.setIcon(icon)
+        self.setIcon(QtGui.QIcon("images\\Microphone_light.png"))
         self.setContextMenu(self.menu)
         self.setToolTip("Статус микрофона")
         self.show()
 
-#    def signalStatus(self):
+    def Unmute(self):
+        hwnd_active = win32gui.GetForegroundWindow()
+        win32api.SendMessage(hwnd_active, WM_APPCOMMAND, None,
+            APPCOMMAND_MICROPHONE_VOLUME_MUTE)
 
+        # win32api.SendMessage(hwnd_active, WM_APPCOMMAND, None,
+        #     APPCOMMAND_MICROPHONE_VOLUME_MUTE)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
