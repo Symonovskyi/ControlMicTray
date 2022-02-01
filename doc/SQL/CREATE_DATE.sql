6ebc11fd116650aad0cea4febb8223af190788c0
Sif-on <e.simonovskiy@gmail.com>
2022-02-01 02:07:36 +0200

    Adding queries to create a database [CMT-39]

---
+++ doc/SQL/CREATE_DATE.sql
@@ -0,0 +1,7 @@
+-- Заполняем таблицы данными.
+INSERT INTO "User" (UserName) VALUES ('{self.__user_name}');
+INSERT INTO "Alerts" (AlertsType, StandardSound, OwnSound) VALUES ('Off', '\Sound\StandardSound.mp3', (NULL));
+INSERT INTO "Autorun" (EnableProgram, EnableMic) VALUES (1, 1);
+INSERT INTO "Hotkey" (HotkeyMic, HotkeyWalkie) VALUES ('Scroll lock', 'HOME');
+INSERT INTO "Settings" (LanguageCode, NightTheme) VALUES ('rus', 1);
+INSERT INTO "About" (ProgramVersion, WebSite, Email, Copyright, UrlPrivacyPolicy) VALUES ('v.1.2022.01.31-alpha', 'https://controlmictray.pp.ua/', 'info@controlmictray.pp.ua', 'Copyright © 2022\nSimonovskiy & Lastivka\nAll rights reserved', 'https://controlmictray.pp.ua/PrivacyPolicy.html');
