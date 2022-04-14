e10225fcbc7473d21263c52ccaa9ac772a8c9735
Sif-on <e.simonovskiy@gmail.com>
2022-04-14 21:56:48 +0300

    Prepare style file

--- ui/styles/styles.py
+++ ui/styles/styles.py
@@ -1,43 +1,362 @@
-class TrayIconStyles:
-    def __init__(self, qinstance):
-        self.tray = qinstance
+class GlobalStyles:
+	border1 = "1px solid"
+	border2 = "0"
+	font_family = "Arial, sans-serif"
+	font_size1 = "14px"
+	font_size2 = "10px"
+	text_align1 = "left"
+	border_radius1 = "3px"
+	border_radius2 = "0"
+	border_radius3 = "5px"
 
-    def dark_theme(self):
-        self.tray.menu.setStyleSheet(
-            """QMenu {
-                color: #7D8A90;
-                background-color: #1F2A30;
-                border: 1px solid #444F55;
-                border-radius: 3px;
-                selection-background-color: #273238;
-                selection-color: #BECBD1;
-            }""")
+	# Dark theme
+	dark_color1 = "#7D8A90"  # Кнопки в трее, обновления, политика, копирайт.
+	dark_color2 = "#1F2A30"
+	dark_color3 = "#444F55"
+	dark_color4 = "#273238"
+	dark_color5 = "#BECBD1"
+	dark_color6 = "#04BED5"
+	dark_color7 = "#CEDCDF"
+	dark_color8 = "#127D91"
+	dark_color9 = "rgba(0, 0, 0, 0)"
 
-    def white_theme(self):
-        self.tray.menu.setStyleSheet(
-            """QMenu { }""")
+	# White theme
+	white_color1 = "#7D8A90"  # Кнопки в трее, обновления, политика, копирайт.
+	white_color2 = "#1F2A30"
+	white_color3 = "#444F55"
+	white_color4 = "#273238"
+	white_color5 = "#BECBD1"
+	white_color6 = "#04BED5"
+	white_color7 = "#CEDCDF"
+	white_color8 = "#127D91"
+	white_color9 = "rgba(0, 0, 0, 0)"
+
+class TrayIconStyles(GlobalStyles):
+	def __init__(self, qinstance):
+		self.tray = qinstance
+
+	def dark_theme(self):
+		self.tray.menu.setStyleSheet(
+			"""QMenu {"""
+				f'color: {self.dark_color1};'
+				f'background-color: {self.dark_color2};'
+				f'border: {self.border1} {self.dark_color3};'
+				f'border-radius: {self.border_radius1};'
+				f'selection-background-color: {self.dark_color4};'
+				f'selection-color: {self.dark_color5};'
+			"""}"""
+		)
+
+	def white_theme(self):
+		self.tray.menu.setStyleSheet(
+			"""QMenu {"""
+				f'color: {self.white_color1};'
+				f'background-color: {self.white_color2};'
+				f'border: {self.border1} {self.white_color3};'
+				f'border-radius: {self.border_radius1};'
+				f'selection-background-color: {self.white_color4};'
+				f'selection-color: {self.white_color5};'
+			"""}"""
+		)
 
 
-class SettingsWindowStyles:
-    def __init__(self, qinstance):
-        self.settings_win = qinstance.settings_UI
+class SettingsWindowStyles(GlobalStyles):
+	def __init__(self, qinstance):
+		self.settings_win_qwidget = qinstance
+		self.settings_win = self.settings_win_qwidget.settings_UI
 
-    def dark_theme(self):
-        # self.settings_win.элемент_управления.setStyleSheet("""
-        # background-color: white;
-        # """)
-        pass
+	def dark_theme(self):
+		self.settings_win_qwidget.setStyleSheet(
+			"""QWidget {"""
+				f'color: {self.dark_color5};'
+				f'background-color: {self.dark_color4};'
+				f'border: {self.border2};'
+				f'font-size: {self.font_size1};'
+				f'font-family: {self.font_family}'
+			"""}"""
+		)
 
-    def white_theme(self):
-        pass
+		self.settings_win.AlertsType.setStyleSheet(
+			"""QComboBox {"""
+				f'border-bottom: {self.border1} {self.dark_color3};'
+			"""}"""
+
+			"""QComboBox::hover{"""
+				f'border-bottom: {self.border1} {self.dark_color6};'
+			"""}"""
+			
+			"""QComboBox QAbstractItemView {"""
+				f'background-color: {self.dark_color2};'
+				f'border: {self.border1} {self.dark_color6};'
+				f'color: {self.dark_color1};'
+				f'selection-background-color: {self.dark_color4};'
+				f'selection-color: {self.dark_color5};'
+				f'border-top-left-radius: {self.border_radius2};'
+				f'border-top-right-radius: {self.border_radius2};'
+				f'border-bottom-right-radius: {self.border_radius3};'
+				f'border-bottom-left-radius: {self.border_radius3};'
+			"""}"""
+		)
+
+		self.settings_win.HotkeyMic.setStyleSheet(
+			"""QLineEdit {"""
+				f'border-bottom: {self.border1} {self.dark_color3};'
+			"""}"""
+
+			"""QLineEdit::hover{"""
+				f'border-bottom: {self.border1} {self.dark_color6};'
+			"""}"""
+		)
+
+		self.settings_win.HotkeyWalkie.setStyleSheet(
+			"""QLineEdit {"""
+				f'border-bottom: {self.border1} {self.dark_color3};'
+			"""}"""
+
+			"""QLineEdit::hover{"""
+				f'border-bottom: {self.border1} {self.dark_color6};'
+			"""}"""
+		)
+
+		self.settings_win.LanguageCode.setStyleSheet(
+			"""QComboBox {"""
+				f'border-bottom: {self.border1} {self.dark_color3};'
+			"""}"""
+
+			"""QComboBox::hover{"""
+				f'border-bottom: {self.border1} {self.dark_color6};'
+			"""}"""
+
+			"""QComboBox QAbstractItemView {"""
+				f'background-color: {self.dark_color2};'
+				f'border: {self.border1} {self.dark_color6};'
+				f'color: {self.dark_color1};'
+				f'selection-background-color: {self.dark_color4};'
+				f'selection-color: {self.dark_color5};'
+				f'border-top-left-radius: {self.border_radius2};'
+				f'border-top-right-radius: {self.border_radius2};'
+				f'border-bottom-right-radius: {self.border_radius3};'
+				f'border-bottom-left-radius: {self.border_radius3};'
+			"""}"""
+		)
+
+		self.settings_win.UrlUpdates.setStyleSheet(
+			"""QPushButton {"""
+				f'color: {self.dark_color1};'
+				f'border-bottom: {self.border1} {self.dark_color2};'
+			"""}"""
+			
+			"""QPushButton::hover {"""
+				f'color: {self.dark_color7};'
+				f'border-bottom: {self.border1} {self.dark_color6};'
+			"""}"""
+		)
+
+	def white_theme(self):
+		self.settings_win_qwidget.setStyleSheet(
+			"""QWidget {"""
+				f'color: {self.white_color5};'
+				f'background-color: {self.white_color4};'
+				f'border: {self.border2};'
+				f'font-size: {self.font_size1};'
+				f'font-family: {self.font_family}'
+			"""}"""
+		)
+
+		self.settings_win.AlertsType.setStyleSheet(
+			"""QComboBox {"""
+				f'border-bottom: {self.border1} {self.white_color3};'
+			"""}"""
+
+			"""QComboBox::hover{"""
+				f'border-bottom: {self.border1} {self.white_color6};'
+			"""}"""
+
+			"""QComboBox QAbstractItemView {"""
+				f'background-color: {self.white_color2};'
+				f'border: {self.border1} {self.white_color6};'
+				f'color: {self.white_color1};'
+				f'selection-background-color: {self.white_color4};'
+				f'selection-color: {self.white_color5};'
+				f'border-top-left-radius: {self.border_radius2};'
+				f'border-top-right-radius: {self.border_radius2};'
+				f'border-bottom-right-radius: {self.border_radius3};'
+				f'border-bottom-left-radius: {self.border_radius3};'
+			"""}"""
+		)
+
+		self.settings_win.HotkeyMic.setStyleSheet(
+			"""QLineEdit {"""
+				f'border-bottom: {self.border1} {self.white_color3};'
+			"""}"""
+
+			"""QLineEdit::hover{"""
+				f'border-bottom: {self.border1} {self.white_color6};'
+			"""}"""
+		)
+
+		self.settings_win.HotkeyWalkie.setStyleSheet(
+			"""QLineEdit {"""
+				f'border-bottom: {self.border1} {self.white_color3};'
+			"""}"""
+
+			"""QLineEdit::hover{"""
+				f'border-bottom: {self.border1} {self.white_color6};'
+			"""}"""
+		)
+
+		self.settings_win.LanguageCode.setStyleSheet(
+			"""QComboBox {"""
+				f'border-bottom: {self.border1} {self.white_color3};'
+			"""}"""
+
+			"""QComboBox::hover{"""
+				f'border-bottom: {self.border1} {self.white_color6};'
+			"""}"""
+
+			"""QComboBox QAbstractItemView {"""
+				f'background-color: {self.white_color2};'
+				f'border: {self.border1} {self.white_color6};'
+				f'color: {self.white_color1};'
+				f'selection-background-color: {self.white_color4};'
+				f'selection-color: {self.white_color5};'
+				f'border-top-left-radius: {self.border_radius2};'
+				f'border-top-right-radius: {self.border_radius2};'
+				f'border-bottom-right-radius: {self.border_radius3};'
+				f'border-bottom-left-radius: {self.border_radius3};'
+			"""}"""
+		)
+
+		self.settings_win.UrlUpdates.setStyleSheet(
+			"""QPushButton {"""
+				f'color: {self.white_color1};'
+				f'border-bottom: {self.border1} {self.white_color2};'
+			"""}"""
+
+			"""QPushButton::hover {"""
+				f'color: {self.white_color7};'
+				f'border-bottom: {self.border1} {self.white_color6};'
+			"""}"""
+		)
 
 
-class AboutWindowStyles:
-    def __init__(self, qinstance):
-        self.about_win = qinstance.about_UI
+class AboutWindowStyles(GlobalStyles):
+	def __init__(self, qinstance):
+		self.about_win_qwidget = qinstance
+		self.about_win = self.about_win_qwidget.about_UI
 
-    def dark_theme(self):
-        pass
+	def dark_theme(self):
+		self.about_win_qwidget.setStyleSheet(
+			"""QWidget {"""
+				f'color: {self.dark_color5};'
+				f'background-color: {self.dark_color4};'
+				f'border: {self.border2};'
+				f'font-size: {self.font_size1};'
+				f'font-family: {self.font_family}'
+			"""}"""
+		)
 
-    def white_theme(self):
-        pass
+		self.about_win.Copyright.setStyleSheet(
+			"""QLabel{"""
+				f'color: {self.dark_color1};'
+				f'font-size: {self.font_size2};'
+			"""}"""
+		)
+
+		self.about_win.Email.setStyleSheet(
+			"""QPushButton {"""
+				f'color: {self.dark_color8};'
+				f'text-align: {self.text_align1};'
+			"""}"""
+			
+			"""QPushButton::hover {"""
+				f'color: {self.dark_color6};'
+			"""}"""
+		)
+
+		self.about_win.LogoFrame.setStyleSheet(
+			"""QLabel{"""
+				f'background-color: {self.dark_color9};'
+			"""}"""
+		)
+
+		self.about_win.UrlPrivacyPolicy.setStyleSheet(
+			"""QPushButton {"""
+				f'color: {self.dark_color1};'
+				f'border-bottom: {self.border1} {self.dark_color2};'
+			"""}"""
+
+			"""QPushButton::hover {"""
+				f'color: {self.dark_color7};'
+				f'border-bottom: {self.border1} {self.dark_color6};'
+			"""}"""
+		)
+
+		self.about_win.WebSite.setStyleSheet(
+			"""QPushButton {"""
+				f'color: {self.dark_color8};'
+				f'text-align: {self.text_align1};'
+			"""}"""
+
+			"""QPushButton::hover {"""
+				f'color: {self.dark_color6};'
+			"""}"""
+		)
+
+	def white_theme(self):
+		self.about_win_qwidget.setStyleSheet(
+			"""QWidget {"""
+				f'color: {self.white_color5};'
+				f'background-color: {self.white_color4};'
+				f'border: {self.border2};'
+				f'font-size: {self.font_size1};'
+				f'font-family: {self.font_family}'
+			"""}"""
+		)
+
+		self.about_win.Copyright.setStyleSheet(
+			"""QLabel{"""
+				f'color: {self.white_color1};'
+				f'font-size: {self.font_size2};'
+			"""}"""
+		)
+
+		self.about_win.Email.setStyleSheet(
+			"""QPushButton {"""
+				f'color: {self.white_color8};'
+				f'text-align: {self.text_align1};'
+			"""}"""
+
+			"""QPushButton::hover {"""
+				f'color: {self.white_color6};'
+			"""}"""
+		)
+
+		self.about_win.LogoFrame.setStyleSheet(
+			"""QLabel{"""
+				f'background-color: {self.white_color9};'
+			"""}"""
+		)
+
+		self.about_win.UrlPrivacyPolicy.setStyleSheet(
+			"""QPushButton {"""
+				f'color: {self.white_color1};'
+				f'border-bottom: {self.border1} {self.white_color2};'
+			"""}"""
+
+			"""QPushButton::hover {"""
+				f'color: {self.white_color7};'
+				f'border-bottom: {self.border1} {self.white_color6};'
+			"""}"""
+		)
+
+		self.about_win.WebSite.setStyleSheet(
+			"""QPushButton {"""
+				f'color: {self.white_color8};'
+				f'text-align: {self.text_align1};'
+			"""}"""
+
+			"""QPushButton::hover {"""
+				f'color: {self.white_color6};'
+			"""}"""
+		)
