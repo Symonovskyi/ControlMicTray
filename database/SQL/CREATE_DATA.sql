116d505ddbb71b5afdf03c5f42b8a7a298a6c341
Sif-on <e.simonovskiy@gmail.com>
2022-04-18 06:56:44 +0300

    Mail and version update

--- database/SQL/CREATE_DATA.sql
+++ database/SQL/CREATE_DATA.sql
@@ -3,4 +3,4 @@ INSERT INTO "Alerts" (AlertsType, StandardSound, OwnSound) VALUES ('Off', '\Soun
 INSERT INTO "Autorun" (EnableProgram, EnableMic, MicStatus, WalkieStatus) VALUES (1, 1, 1, 0);
 INSERT INTO "Hotkey" (HotkeyMic, HotkeyWalkie) VALUES ('Ctrl+Shift+/', 'Scroll_lock');
 INSERT INTO "Settings" (LanguageCode, NightTheme, PrivacyStatus) VALUES ('rus', 1, 0);
-INSERT INTO "About" (ProgramVersion, WebSite, Email, Copyright, UrlPrivacyPolicy) VALUES ('v.1.2022.02.14', 'https://controlmictray.pp.ua/', 'info@controlmictray.pp.ua', 'Copyright © 2022\nSimonovskiy & Lastivka\nAll rights reserved', 'https://controlmictray.pp.ua/PrivacyPolicy.html');
+INSERT INTO "About" (ProgramVersion, WebSite, Email, Copyright, UrlPrivacyPolicy) VALUES ('v.2022.04.18', 'https://controlmictray.pp.ua/', 'i@controlmictray.pp.ua', 'Copyright © 2022\nSimonovskiy & Lastivka\nAll rights reserved', 'https://controlmictray.pp.ua/PrivacyPolicy.html');
