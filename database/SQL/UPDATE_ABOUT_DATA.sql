2fd0d4ac12582cd3a628e0426fabd94d3d5cc8ef
Sif-on <e.simonovskiy@gmail.com>
2022-04-20 12:51:48 +0300

    Update SQL

--- database/SQL/UPDATE_ABOUT_DATA.sql
+++ database/SQL/UPDATE_ABOUT_DATA.sql
@@ -1,4 +1,4 @@
 -- Update data in the ABOUT table.
 UPDATE "About" 
 SET (ProgramVersion, WebSite, Email, Copyright, UrlPrivacyPolicy) 
-= ('v.2022.04.18', 'https://controlmictray.pp.ua/', 'i@controlmictray.pp.ua', 'Copyright © 2022\nSimonovskiy & Lastivka\nAll rights reserved', 'https://controlmictray.pp.ua/PrivacyPolicy.html');
+= ('v.2022.04.18', 'controlmictray.pp.ua', 'i@controlmictray.pp.ua', 'Copyright © 2022\nSimonovskiy & Lastivka\nAll rights reserved', 'https://controlmictray.pp.ua/PrivacyPolicy.html');
